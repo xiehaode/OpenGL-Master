8e6d721b4ae45496a3b763f4b12ca94bd0437286
xiehaode <2802625868@qq.com>
2026-03-04 20:53:16 +0800

    add imgui

--- include/mText.h
+++ include/mText.h
@@ -15,15 +15,14 @@ public:
     mText(const std::string& fontPath = "../resources/fonts/Antonio-Bold.ttf");
     ~mText();
 
-    // 渲染文本 - 简化版本，只在控制台输出
+
     void draw(const std::string& text, float x, float y, float scale,
               const glm::vec3& color, glm::mat4 projection) {
-        // 简化实现，只在控制台输出分数
-        std::cout << "Score: " << text << std::endl;
+
     }
 
 private:
-    // 简化实现，不需要实际的文本渲染
+
 };
 
 #endif //MTEXT_H
